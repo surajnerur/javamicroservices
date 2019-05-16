fa3be8c2c4a459552ed5f9b251169089d9880348
Suraj Nerurkar <suraj.nerurkar@tieto.com>
2019-05-16 19:32:29 +0530

    updated by Suraj

--- microservicesservicediscoveryway/movie-catalog-service/src/main/java/io/microservices/moviecatalogservice/resource/MovieCatalogResource.java
+++ microservicesservicediscoveryway/movie-catalog-service/src/main/java/io/microservices/moviecatalogservice/resource/MovieCatalogResource.java
@@ -6,6 +6,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -29,6 +30,9 @@ public class MovieCatalogResource {
 	@Autowired
 	private WebClient.Builder webClientBuilder;
 	
+	@Autowired
+	private DiscoveryClient discoveryClient;
+	
 	@RequestMapping("/{userId}")
 	public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
 		
@@ -36,10 +40,12 @@ public class MovieCatalogResource {
 				new Rating("1234",4),
 				new Rating("5678",3)
 			);*/
-		UserRating userRating = restTemplate.getForObject("http://localhost:8083/ratingsData/users/"+userId, UserRating.class);
+		//UserRating userRating = restTemplate.getForObject("http://localhost:8083/ratingsData/users/"+userId, UserRating.class);
+		UserRating userRating = restTemplate.getForObject("http://ratings-data-service/ratingsData/users/"+userId, UserRating.class);
 		
 		return userRating.getRatings().stream().map(rating -> {
-			Movie movie = restTemplate.getForObject("http://localhost:8082/movies/"+rating.getMovieId(), Movie.class);
+			//Movie movie = restTemplate.getForObject("http://localhost:8082/movies/"+rating.getMovieId(), Movie.class);
+			Movie movie = restTemplate.getForObject("http://movie-info-service/movies/"+rating.getMovieId(), Movie.class);
 			return new CatalogItem(movie.getName(),"Test",rating.getRating());
 			}).collect(Collectors.toList());
 		
