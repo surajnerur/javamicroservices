936c7cc1be57b82b1fb2693b9c9aa48bb1d4d475
Suraj Nerurkar <suraj.nerurkar@tieto.com>
2019-08-29 17:36:17 +0530

    updated by Suraj

--- microservicesbeanvalidationframework/dicovery-server-beanvalidation/src/main/java/io/microservices/beanvalidation/discoveryserver/DicoveryServerBeanvalidationApplication.java
+++ microservicesbeanvalidationframework/dicovery-server-beanvalidation/src/main/java/io/microservices/beanvalidation/discoveryserver/DicoveryServerBeanvalidationApplication.java
@@ -2,8 +2,10 @@ package io.microservices.beanvalidation.discoveryserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class DicoveryServerBeanvalidationApplication {
 
 	public static void main(String[] args) {
--- microservicesbeanvalidationframework/microservicesbeanvalidation/src/main/java/io/microservices/beanvalidation/MicroservicesbeanvalidationApplication.java
+++ microservicesbeanvalidationframework/microservicesbeanvalidation/src/main/java/io/microservices/beanvalidation/MicroservicesbeanvalidationApplication.java
@@ -2,8 +2,10 @@ package io.microservices.beanvalidation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class MicroservicesbeanvalidationApplication {
 
 	public static void main(String[] args) {
