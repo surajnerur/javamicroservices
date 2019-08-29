3c0078bef264f112f41eea68297a4548c49077b1
Suraj Nerurkar <suraj.nerurkar@tieto.com>
2019-08-29 13:53:30 +0530

    updated by Suraj

---
+++ microservicesbeanvalidationframework/dicovery-server-beanvalidation/src/main/java/io/microservices/beanvalidation/discoveryserver/DicoveryServerBeanvalidationApplication.java
@@ -0,0 +1,13 @@
+package io.microservices.beanvalidation.discoveryserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DicoveryServerBeanvalidationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DicoveryServerBeanvalidationApplication.class, args);
+	}
+
+}
